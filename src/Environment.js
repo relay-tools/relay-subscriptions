a2faab4f225462276967efa388d8cf1968bbca29
voxmatt <matt@clockwisehq.com>
2017-06-06 15:28:39 -0700

    fix collision with compat subscribe method (not sure about subscribeClassic as the method name though)

--- src/Environment.js
+++ src/Environment.js
@@ -40,7 +40,7 @@ class StoreData extends RelayStoreData {
 }
 
 export default class Environment extends Relay.Environment {
-  subscribe: (
+  subscribeClassic: (
     subscription: Subscription<any>,
     observer?: SubscriptionObserver,
   ) => SubscriptionDisposable;
@@ -48,12 +48,12 @@ export default class Environment extends Relay.Environment {
   constructor(storeData?: StoreData) {
     super(storeData || new StoreData());
 
-    this.subscribe = this.subscribe.bind(this);
+    this.subscribeClassic = this.subscribeClassic.bind(this);
 
     this._nextClientSubscriptionId = 0;
   }
 
-  subscribe(
+  subscribeClassic(
     subscription: Subscription<any>,
     observer?: SubscriptionObserver,
   ): SubscriptionDisposable {
