034c995474b63e1a0d3f60165d409b897cc5250a
Jimmy Jia <tesrin@gmail.com>
2018-01-05 08:35:47 -0500

    Use subscriptions-transport-ws (#40)

--- examples/todo/js/NetworkLayer.js
+++ examples/todo/js/NetworkLayer.js
@@ -1,69 +1,38 @@
 /* eslint-disable no-console */
 
 import Relay from 'react-relay/classic';
-import io from 'socket.io-client';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
 
 export default class NetworkLayer extends Relay.DefaultNetworkLayer {
   constructor(...args) {
     super(...args);
 
-    this._socket = io();
-    this._requests = Object.create(null);
-
-    this._socket.on('subscription update', ({ id, data, errors }) => {
-      const request = this._requests[id];
-      if (!request) {
-        return;
-      }
-
-      if (errors) {
-        request.onError(errors);
-      } else {
-        request.onNext(data);
-      }
-    });
-
-    this._socket.on('subscription closed', (id) => {
-      const request = this._requests[id];
-      if (!request) {
-        return;
-      }
-
-      console.log(`Subscription ${id} is completed`);
-      request.onCompleted();
-      delete this._requests[id];
-    });
-
-    this._socket.on('error', (error) => {
-      Object.values(this._requests).forEach((request) => {
-        request.onError(error);
-      });
-    });
+    this._subscriptionClient = new SubscriptionClient(
+      `ws://${global.location.host}/graphql`,
+      { reconnect: true },
+    );
   }
 
   sendSubscription(request) {
-    const id = request.getClientSubscriptionId();
-    this._requests[id] = request;
-
-    this._socket.emit('subscribe', {
-      id,
+    const { unsubscribe } = this._subscriptionClient.request({
       query: request.getQueryString(),
       variables: request.getVariables(),
+    }).subscribe({
+      next: ({ errors, data }) => {
+        if (errors) {
+          request.onError(errors);
+        } else {
+          request.onNext(data);
+        }
+      },
+      error: request.onError,
+      complete: request.onCompleted,
     });
 
-    return {
-      dispose: () => {
-        console.log(`disposing ${request.getDebugName()}:${id}`);
-        this._socket.emit('unsubscribe', id);
-      },
-    };
+    return { dispose: unsubscribe };
   }
 
   disconnect() {
-    this._socket.disconnect();
-
-    this._requests.forEach(request => {
-      request.onCompleted();
-    });
+    this._subscriptionClient.close();
   }
 }
